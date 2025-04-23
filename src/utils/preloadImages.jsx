2a67e91bb87529c7c9bf100935d5a6773244362e
Colt Fulk <coltfulk@gmail.com>
2025-04-23 00:13:11 -0700

    preload update

--- src/utils/preloadImages.jsx
+++ src/utils/preloadImages.jsx
@@ -2,5 +2,6 @@ export const preloadImages = (imageUrls) => {
   imageUrls.forEach((url) => {
     const img = new Image();
     img.src = url;
+    console.log(`Preloading image: ${url}`); // Log each image URL
   });
 };
