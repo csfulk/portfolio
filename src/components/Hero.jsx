0aca4845429bde33702a3400aed5f7c633428e13
Colt Fulk <coltfulk@gmail.com>
2025-08-28 21:55:59 -0700

    update to hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="hero">
       <div className="content-wrapper">
         <h1 className="title">
-          Colt uses design thinking to craft world-class UI systems
+          Colt uses design thinking to craft robust, world-class UI systems
         </h1>
         
         <div className='detail-wrapper'>
